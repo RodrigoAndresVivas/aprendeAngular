a3fe9be2e70a3c249e744f35df8101340b0a6501
RodrigoAndresVivas <rodrigovivash@gmail.com>
2019-02-15 21:48:11 -0500

    Primer Paso al Comit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { PagesComponent } from './pages/pages.component';
+import { PagesfalledComponent } from './pagesfalled/pagesfalled.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { RegisterComponent } from './login/register.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      {path: 'dashboard', component: DashboardComponent},
+      {path: 'progress', component: ProgressComponent},
+      {path: 'grafiacas1', component: Graficas1Component},
+      {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+    ]
+  },
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: '**', component: PagesfalledComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pagesfalled/pagesfalled.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagesfalled',
+  templateUrl: './pagesfalled.component.html',
+  styles: []
+})
+export class PagesfalledComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
